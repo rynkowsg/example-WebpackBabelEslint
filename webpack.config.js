42a3ed1fa48277f28e4a5a01de16cd0c63dd27e1
Greg Rynkowski <dev@rynkowski.info>
2017-08-06 10:53:23 +0100

    [tutorial] Add auto-generating index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const config = {
   entry: {
     app: './src/index.js',
-    print: './src/print.js'
+    print: './src/print.js',
   },
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
 };
 
 module.exports = config;
