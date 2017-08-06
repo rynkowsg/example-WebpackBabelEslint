60549f9afd2adba59678dbc13f7a48c71dc2485b
Greg Rynkowski <dev@rynkowski.info>
2017-08-06 11:04:11 +0100

    [tutorial] Add 'print' module

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
 const path = require('path');
 
-module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist')
-    }
+const config = {
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
 };
+
+module.exports = config;
